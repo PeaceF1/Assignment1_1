e7a3efefa3728603cad4662132dd77035f3f60db
PeaceF1 <77022147+PeaceF1@users.noreply.github.com>
2021-05-14 09:40:39 -0500

    intial commit

---
+++ src/assignment2.java
@@ -0,0 +1,21 @@
+public class assignment2 {
+    public static void main(String[] args){
+        System.out.println("     J      A      V       A");
+        System.out.println("    JJJ     AAAA    VVVV    AAAAAA");
+        System.out.println("   JJJJ    AAAAAAAA   VVV     AAAA");
+        System.out.println("  JJJJ   A       V         A    "); 
+        System.out.println(" JJJ  AAA      VVVV         A"); 
+        int cadence = 0 ;
+        System.out.println(cadence); 
+        int omaha = 2000;
+        System.out.println(omaha); 
+        omaha = omaha + 5 ;
+        System.out.println(omaha); 
+        int speedlimit = 65; 
+        float speed, speed3;
+        speed = 65;
+        speed3 = speed/3; 
+        System.out.println(speed3);  
+    }
+    
+}
---
+++ src/myInfo.java
@@ -0,0 +1,10 @@
+public class myInfo {
+    public static void main(String[] args){
+        System.out.println("Peace Kanu-Asiegbu");
+        System.out.println("Full Stack Java 2021");
+        System.out.println("Omaha, NE");
+        System.out.println("Homemade Icecream");
+    }
+
+    
+}
